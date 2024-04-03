f203217b2dc4e0d48e257c820e7a6db0e4b2095e
gzachr <zachary_gomez@dlsu.edu.ph>
2024-04-03 21:18:43 +0800

    connect central node

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ central_node.connect((err) => {
       console.error('Error connecting to MySQL: ' + err.stack);
       return;
     }
-    console.log('Connected to MySQL as id ' + connection.threadId);
+    console.log('Connected to MySQL as id ');
 });
 
 app.listen(8080, () => {
--- models/nodes.js
+++ models/nodes.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const central_node = mysql.createConnection({
   host: 'ccscloud.dlsu.edu.ph',
   user: 'root',
   password: 'F2qmexnhkb8GYjENHB5zyJaV',
-  database: 'mco2'
+  database: 'mco2',
+  port: 20144
 });
 
 module.exports = central_node;
