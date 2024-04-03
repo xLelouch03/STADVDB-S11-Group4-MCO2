1ecb5a5d492d93d99c48d7769f0c6d9e146d385c
gzachr <zachary_gomez@dlsu.edu.ph>
2024-04-03 20:23:50 +0800

    setup base project

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+
+const app = express();
+
+app.engine('hbs', exphbs.engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use('/static', express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
+});
