03db7f79413c71993169fa7f10f0357d5957740e
Cornars <85560422+Cornars@users.noreply.github.com>
2024-04-09 15:36:44 +0800

    Update sync.js

--- models/sync.js
+++ models/sync.js
@@ -36,40 +36,60 @@ const sync_funcs = {
                 for (let i = 0; i < logs.length; i++) {
                     switch (logs[i].t_type) {
                         case 'INSERT':
-                            const pxid = logs[i].pxid ? `'${logs[i].pxid}'` : null;
-                            const clinicid = logs[i].clinicid ? `'${logs[i].clinicid}'` : null;
-                            const doctorid = logs[i].doctorid ? `'${logs[i].doctorid}'` : null;
-                            const apptid = logs[i].apptid ? `'${logs[i].apptid}'` : null;
-                            const status = logs[i].status ? `'${logs[i].status}'` : null;
-                            const timeQueued = logs[i].TimeQueued ? `'${new Date(logs[i].TimeQueued).toISOString().slice(0, 19).replace('T', ' ')}'` : null;
-                            const queueDate = logs[i].QueueDate ? `'${new Date(logs[i].QueueDate).toISOString().slice(0, 19).replace('T', ' ')}'` : null;
-                            const startTime = logs[i].StartTime ? `'${new Date(logs[i].StartTime).toISOString().slice(0, 19).replace('T', ' ')}'` : null;
-                            const endTime = logs[i].EndTime ? `'${new Date(logs[i].EndTime).toISOString().slice(0, 19).replace('T', ' ')}'` : null;
-                            const type = logs[i].type ? `'${logs[i].type}'` : null;
-                            const isVirtual = logs[i].IsVirtual ? `'${logs[i].IsVirtual}'` : null;
-                            const mainSpecialty = logs[i].mainspecialty ? `'${logs[i].mainspecialty}'` : null;
-                            const hospitalName = logs[i].hospitalname ? `'${logs[i].hospitalname}'` : null;
-                            const isHospital = logs[i].IsHospital ? `'${logs[i].IsHospital}'` : null;
-                            const city = logs[i].City ? `'${logs[i].City}'` : null;
-                            const province = logs[i].Province ? `'${logs[i].Province}'` : null;
-                            const regionName = logs[i].RegionName ? `'${logs[i].RegionName}'` : null;
-                            const patientAge = logs[i].patient_age ? `'${logs[i].patient_age}'` : null;
-                            const patientGender = logs[i].patient_gender ? `'${logs[i].patient_gender}'` : null;
-                            const location = logs[i].Location ? `'${logs[i].Location}'` : null;
+                            var pxid = logs[i].pxid ? `'${logs[i].pxid}'` : null;
+                            var doctorid = logs[i].doctorid ? `'${logs[i].doctorid}'` : null;
+                            var clinicid = logs[i].clinicid ? `'${logs[i].clinicid}'` : null;
+                            var apptid = logs[i].apptid ? `'${logs[i].apptid}'` : null;
+                            var status = logs[i].status ? `'${logs[i].status}'` : null;
+                            var timeQueued = logs[i].TimeQueued ? `'${new Date(logs[i].TimeQueued).toISOString().slice(0, 19).replace('T', ' ')}'` : null;
+                            var queueDate = logs[i].QueueDate ? `'${new Date(logs[i].QueueDate).toISOString().slice(0, 19).replace('T', ' ')}'` : null;
+                            var startTime = logs[i].StartTime ? `'${new Date(logs[i].StartTime).toISOString().slice(0, 19).replace('T', ' ')}'` : null;
+                            var endTime = logs[i].EndTime ? `'${new Date(logs[i].EndTime).toISOString().slice(0, 19).replace('T', ' ')}'` : null;
+                            var type = logs[i].type ? `'${logs[i].type}'` : null;
+                            var isVirtual = logs[i].IsVirtual ? `'${logs[i].IsVirtual}'` : null;
+                            var mainSpecialty = logs[i].mainspecialty ? `'${logs[i].mainspecialty}'` : null;
+                            var hospitalName = logs[i].hospitalname ? `'${logs[i].hospitalname}'` : null;
+                            var isHospital = logs[i].IsHospital ? `'${logs[i].IsHospital}'` : null;
+                            var city = logs[i].City ? `'${logs[i].City}'` : null;
+                            var province = logs[i].Province ? `'${logs[i].Province}'` : null;
+                            var regionName = logs[i].RegionName ? `'${logs[i].RegionName}'` : null;
+                            var patientAge = logs[i].patient_age ? `'${logs[i].patient_age}'` : null;
+                            var patientGender = logs[i].patient_gender ? `'${logs[i].patient_gender}'` : null;
+                            var location = logs[i].Location ? `'${logs[i].Location}'` : null;
 
-                            const query = await queryHelper.to_insert_query(pxid, clinicid, doctorid, apptid, status, timeQueued, queueDate, startTime, endTime, type, isVirtual, mainSpecialty, hospitalName, isHospital, city, province, regionName, patientAge, patientGender, location);
+                            var query = await queryHelper.to_insert_query(pxid, clinicid, doctorid, apptid, status, timeQueued, queueDate, startTime, endTime, type, isVirtual, mainSpecialty, hospitalName, isHospital, city, province, regionName, patientAge, patientGender, location);
                             
-                            const finished_log = await queryHelper.to_finish_log(logs[i].logid);
+                            var finished_log = await queryHelper.to_finish_log(logs[i].logid);
                             tx.non_update_tx(primary_node, finished_log)
-                            const final_result = await tx.non_update_tx(primary_node, query);
+                            var final_result = await tx.non_update_tx(primary_node, query);
 
-                        // case 'UPDATE':
-                        //     query = queryHelper.to_update_query(logs[i].id + "," + "'" + logs[i].pxid + "'", "'" + logs[i].clinicid+"'", "'" + logs[i].doctorid+"'", "'" + logs[i].apptid+"'", "'" + logs[i].status+"'", "'" + new Date(logs[i].TimeQueued).toISOString().slice(0, 19).replace('T', ' ')+"'", "'" + new Date(logs[i].QueueDate).toISOString().slice(0, 19).replace('T', ' ')+"'", 
-                        //     "'" + new Date(logs[i].StartTime).toISOString().slice(0, 19).replace('T', ' ')+"'", "'" + new Date(logs[i].EndTime).toISOString().slice(0, 19).replace('T', ' ')+"'", "'" + logs[i].type+"'", "'" + logs[i].IsVirtual+"'", "'" + logs[i].mainspecialty+"'", "'" + logs[i].hospitalname+"'", "'" + logs[i].IsHospital+"'", "'" + logs[i].City+"'", "'" + logs[i].Province+"'", "'" + logs[i].RegionName+"'", "'" + logs[i].patient_age+"'", "'" + logs[i].patient_gender+"'", "'" + logs[i].Location+"'");
+                        case 'UPDATE':
+                            var pxid = logs[i].pxid ? `'${logs[i].pxid}'` : null;
+                            var clinicid = logs[i].clinicid ? `'${logs[i].clinicid}'` : null;
+                            var doctorid = logs[i].doctorid ? `'${logs[i].doctorid}'` : null;
+                            var apptid = logs[i].apptid ? `'${logs[i].apptid}'` : null;
+                            var status = logs[i].status ? `'${logs[i].status}'` : null;
+                            var timeQueued = logs[i].TimeQueued ? `'${new Date(logs[i].TimeQueued).toISOString().slice(0, 19).replace('T', ' ')}'` : null;
+                            var queueDate = logs[i].QueueDate ? `'${new Date(logs[i].QueueDate).toISOString().slice(0, 19).replace('T', ' ')}'` : null;
+                            var startTime = logs[i].StartTime ? `'${new Date(logs[i].StartTime).toISOString().slice(0, 19).replace('T', ' ')}'` : null;
+                            var endTime = logs[i].EndTime ? `'${new Date(logs[i].EndTime).toISOString().slice(0, 19).replace('T', ' ')}'` : null;
+                            var type = logs[i].type ? `'${logs[i].type}'` : null;
+                            var isVirtual = logs[i].IsVirtual ? `'${logs[i].IsVirtual}'` : null;
+                            var mainSpecialty = logs[i].mainspecialty ? `'${logs[i].mainspecialty}'` : null;
+                            var hospitalName = logs[i].hospitalname ? `'${logs[i].hospitalname}'` : null;
+                            var isHospital = logs[i].IsHospital ? `'${logs[i].IsHospital}'` : null;
+                            var city = logs[i].City ? `'${logs[i].City}'` : null;
+                            var province = logs[i].Province ? `'${logs[i].Province}'` : null;
+                            var regionName = logs[i].RegionName ? `'${logs[i].RegionName}'` : null;
+                            var patientAge = logs[i].patient_age ? `'${logs[i].patient_age}'` : null;
+                            var patientGender = logs[i].patient_gender ? `'${logs[i].patient_gender}'` : null;
+                            var location = logs[i].Location ? `'${logs[i].Location}'` : null;
+
+                            var query = await queryHelper.to_update_query(logs[i].id, pxid, clinicid, doctorid, apptid, status, timeQueued, queueDate, startTime, endTime, type, isVirtual, mainSpecialty, hospitalName, isHospital, city, province, regionName, patientAge, patientGender, location);
                             
-                        //     let update = queryHelper.to_finish_log(logs[i].statement_id);
-                        //     let result = await transaction.make_2transaction(selectedNode, query, update, 'UPDATE', logs[i].id, logs[i].node_from);
-                        //     return (result instanceof Error) ? false : true;
+                            var finished_log = await queryHelper.to_finish_log(logs[i].logid);
+                            tx.non_update_tx(primary_node, finished_log)
+                            var final_result = await tx.non_update_tx(primary_node, query);
 
                         // case 'DELETE':
                         //     query = queryHelper.to_delete_query(logs[i].id);
@@ -86,58 +106,6 @@ const sync_funcs = {
             console.log(error)
             return false;
         }
-    },
-
-    // sync_follower_node: async function (node) {
-    //     let logs = [];
-    //     let logs2 = [];
-    //     let logs3 = [];
-
-    //     try {
-    //         var other = (node === 2) ? 3 : 2;
-
-    //         if (await ping_node(1))  logs2 = await query_node(1, queryHelper.to_retrieve_logs(node));
-    //         if (await ping_node(other)) logs3 = await query_node(other, queryHelper.to_retrieve_logs(node));
-            
-    //         if (logs2[0]) logs = logs2[0];
-    //         if (logs3[0] && logs) logs = logs.concat(logs3[0]);
-    //         else if (logs3[0]) logs = logs3[0];
-            
-    //         if (logs) {
-    //             for (let i = 0; i < logs.length; i++) {
-    //                 if (await ping_node(logs[i].node_to)) {
-    //                     let query;
-    //                     switch (logs[i].type) {
-    //                         case 'INSERT':
-    //                             if (logs[i].id) 
-    //                                 query = queryHelper.to_insert_query_with_id(logs[i].id, logs[i].name, logs[i].rank, logs[i].year);
-    //                             else 
-    //                                 query = queryHelper.to_insert_query(logs[i].name, logs[i].rank, logs[i].year);
-    //                             break;
-    //                         case 'UPDATE':
-                                
-    //                             if (logs[i].new_id) {
-    //                                 query = queryHelper.to_update_id_query(logs[i].new_id, logs[i].id);
-    //                             }
-    //                             else {
-    //                                 query = queryHelper.to_update_query(logs[i].id, logs[i].name, logs[i].rank, logs[i].year);
-    //                             }
-    //                             break;
-                                
-    //                         case 'DELETE':
-    //                             query = queryHelper.to_delete_query(logs[i].id); break;
-    //                     }
-    //                     var result = await transaction.insert_update_transaction(logs[i].node_to, query, queryHelper.to_finish_log(logs[i].statement_id), logs[i].node_from, logs[i].type, logs[i].id);
-    //                     console.log('Synced to Node ' + node);
-    //                     return (result instanceof Error) ? false : true;
-    //                 }
-    //             }
-    //         }
-    //         return true;
-    //     }
-    //     catch (error) {
-    //         console.log(error)
-    //     }
-    // }
+    }
 }
 module.exports = sync_funcs;
