1ceffeb66af0bda2f55471131f0c1bc3efc31664
Cornars <85560422+Cornars@users.noreply.github.com>
2024-04-10 17:22:06 +0800

    test: Add isolation level for test

--- tests/db.test.js
+++ tests/db.test.js
@@ -51,6 +51,7 @@ test('Concurrent transactions in two or more nodes are reading the same data ite
   await conn_for_deleting.query(`DELETE FROM appointments WHERE apptid LIKE 'test'`);
   await db.insert_query(data);
   const transaction1 = async () => {
+    
     conn = await nodes.get_single_connection('10.2.0.144');
     await conn.beginTransaction();
     await conn.query(`SET @@session.time_zone = "+08:00";`);
@@ -74,7 +75,7 @@ test('Concurrent transactions in two or more nodes are reading the same data ite
   // Execute both transactions concurrently
   await Promise.all([transaction1(), transaction2()]);
   await conn_for_deleting.query(`DELETE FROM appointments WHERE apptid LIKE 'test'`);
-});
+}, 10000);
 
 test('At least one transaction in the three nodes is writing (update / delete) and the other concurrent transactions are reading the same data item.', async () => {
   // Define the concurrent transactions to be tested
@@ -109,21 +110,32 @@ test('At least one transaction in the three nodes is writing (update / delete) a
   let result2 = await conn.query("SELECT id from appointments where apptid LIKE 'test'");
 
   const transaction1 = async () => {
-    conn.beginTransaction();
-    conn.query("DO SLEEP(3)");
+    await conn.query('SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;');
+    await conn.beginTransaction();
+    console.log("TRANSACTION 1 HAS STARTED");
+    result3 = await conn.query(`SELECT hospitalname FROM appointments WHERE id = ${result2[0][0].id}`);
+    console.log('result 3: ', result3);
+    await conn.query("DO SLEEP(3)");
+    console.log("SLEEP STARTED");
     result = await conn.query(`SELECT hospitalname FROM appointments WHERE id = ${result2[0][0].id}`);
-    conn.commit();
-    conn.release();
-    console.log('test', result)
-    expect(result[0][0].hospitalname).toBe('Rejano Hospital');
+    console.log('result: ', result);
+    await conn.commit();
+    await conn.release();
+    console.log("TRANSACTION 1 HAS ENDED");
+    expect(result3[0][0].hospitalname).toBe('Bawa Hospital');
+    expect(result[0][0].hospitalname).toBe('Bawa Hospital');
   };
 
   const transaction2 = async () => {
+    
+    await new Promise(resolve => setTimeout(resolve, 500));
+    console.log("TRANSACTION 2 HAS STARTED");
     db.update_query(data_updated, result2[0][0].id);
+    console.log("TRANSACTION 2 HAS ENDED");
   };
 
   // Execute both transactions concurrently
   await Promise.all([transaction1(), transaction2()]);
   await conn_for_deleting.query(`DELETE FROM appointments WHERE apptid LIKE 'test'`);
-});
+}, 100000);
 
