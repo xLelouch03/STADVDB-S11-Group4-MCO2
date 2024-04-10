cef2d80794cd8824719fc05ca3b9ded848b64176
Francis Isip <francis_bawa@dlsu.edu.ph>
2024-04-10 22:13:09 +0800

    color change status

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,23 @@ async function fetchNodeStatus() {
         document.getElementById("node2Status").innerText = data.node2Connected ? "Online" : "Offline";
         document.getElementById("node3Status").innerText = data.node3Connected ? "Online" : "Offline";
 
+        if (document.getElementById("node1Status").innerText === "Online") {
+            document.getElementById("node1Status").classList.add("text-success");
+        } else {
+            document.getElementById("node1Status").classList.add("text-danger");
+        }
+
+        if (document.getElementById("node2Status").innerText === "Online") {
+            document.getElementById("node2Status").classList.add("text-success");
+        } else {
+            document.getElementById("node2Status").classList.add("text-danger");
+        }
+
+        if (document.getElementById("node3Status").innerText === "Online") {
+            document.getElementById("node3Status").classList.add("text-success");
+        } else {
+            document.getElementById("node3Status").classList.add("text-danger");
+        }
         
     } catch (error) {
         console.error('Error fetching node status:', error);
